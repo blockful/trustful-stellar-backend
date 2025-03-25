3dbf4c1fa942fbb29e7342d058958b5b40046ea3
Leonardo Vieira <jonaledi@gmail.com>
2025-03-25 08:51:18 -0300

    refactor: createBadge to handle new data variables

--- src/badges/badges.service.ts
+++ src/badges/badges.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { BadgeDto } from './dto/badge.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class BadgesService {
@@ -21,8 +22,28 @@ export class BadgesService {
   }
 
   async createBadge(badgeDto: BadgeDto) {
+    const community = await this.prisma.community.findUnique({
+      where: {
+        community_address: badgeDto.communityAddress
+      }
+    });
+
+    if (!community) {
+      throw new NotFoundException(`Community with address ${badgeDto.communityAddress} not found`);
+    }
+
     return this.prisma.badge.create({
-      data: badgeDto
+      data: {
+        id: uuidv4(),
+        issuer: badgeDto.issuer,
+        community_address: badgeDto.communityAddress,
+        name: badgeDto.name,
+        score: badgeDto.score,
+        type: badgeDto.type,
+        created_at: badgeDto.createdAt ? new Date(badgeDto.createdAt).getTime() : null,
+        removed_at: badgeDto.removedAt ? new Date(badgeDto.removedAt).getTime() : null,
+        community_id: community.id,
+      }
     });
   }
 }
